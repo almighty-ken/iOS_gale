6aaa46c2eb4c7c8cc8904efa1901a9a4aa1724ff
kencheng <kenny851017@gmail.com>
2016-11-28 00:13:29 -0500

    added POST prototype

--- Gale/Gale/LogInViewController.swift
+++ Gale/Gale/LogInViewController.swift
@@ -10,6 +10,38 @@ import UIKit
 
 class LogInViewController: UIViewController {
 
+    @IBOutlet weak var user_name: UITextField!
+    @IBOutlet weak var user_password: UITextField!
+    
+    @IBAction func log_in(_ sender: Any) {
+        let url = URL(string: "http://localhost:4000/api/login")
+        var request = URLRequest(url: url!)
+        request.setValue("application/json; charset=utf-8", forHTTPHeaderField: "Content-Type")  // the request is JSON
+        //request.setValue("application/json; charset=utf-8", forHTTPHeaderField: "Accept")        // the expected response is also JSON
+        request.httpMethod = "POST"
+        
+        let dictionary = ["username": user_name, "password": user_password]
+        request.httpBody = try! JSONSerialization.data(withJSONObject: dictionary)
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data, error == nil else {
+                print(error!)                                 // some fundamental network error
+                return
+            }
+            
+            do {
+                let responseObject = try JSONSerialization.jsonObject(with: data)
+                print(responseObject)
+            } catch let jsonError {
+                print(jsonError)
+                print(String(data: data, encoding: .utf8)!)   // often the `data` contains informative description of the nature of the error, so let's look at that, too
+            }
+        }
+        task.resume()
+        // TODO: Take response and decide segue
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
